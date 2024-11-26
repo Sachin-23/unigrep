24450fea2b2fcb6e393cc5032b042c928d48d006
Preet Soni <preetssoni99@gmail.com>
2024-11-27 00:02:08 +0530

    download option rectified

--- frontend/unigrep/src/components/Tab2/Tab2.js
+++ frontend/unigrep/src/components/Tab2/Tab2.js
@@ -7,7 +7,7 @@ import { performApply } from '../utils/Apply';
 
 const Tab2 = () => {
   const { selectedFileType, selectedConnection, selectedMode, searchResult, root, username, password } = useTabState();
-  const [operation, setOperation] = useState(selectedConnection === "ftp" ? "download" : "delete");
+  const [operation, setOperation] = useState(selectedConnection === "local" ? "delete" : "download");
   const [location, setLocation] = useState("/destination/location");
   const handleOperationChange = (e) => {
     setOperation(e.target.value);
